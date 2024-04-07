86eff920d9d30fbaad4b3b38daead0fb159e14fe
Selman <selmank@gmail.com>
2024-04-07 17:28:35 +0200

    docs: add docstring

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+"""
+This module contains a Flask application for a machine learning model.
+The application provides an endpoint to make predictions using the model.
+The model is trained to predict housing prices and is loaded from a joblib file.
+"""
+
+import logging
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
-import logging
 
 import pandas as pd
 import joblib
@@ -10,6 +16,7 @@ app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
+
 def scale(payload):
     """Scales Payload"""
 
@@ -18,21 +25,26 @@ def scale(payload):
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
 
+
 @app.route("/")
 def home():
+    """Home"""
     html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
+
 # TO DO:  Log out the prediction value
-@app.route("/predict", methods=['POST'])
+
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    # Performs an sklearn prediction
+    """Performs an sklearn prediction"""
     try:
-        # Load pretrained model as clf. Try any one model. 
+        # Load pretrained model as clf. Try any one model.
         # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
         clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
-    except:
+    except FileNotFoundError:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
@@ -42,7 +54,8 @@ def predict():
     LOG.info("inference payload DataFrame: %s inference_payload")
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
-    return jsonify({'prediction': prediction})
+    return jsonify({"prediction": prediction})
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
