97e1deaeb94afbbc6080288e91c81065bc66fe25
Selman <selmank@gmail.com>
2024-04-11 19:50:06 +0200

    Update h3 text in app.py home function

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def scale(payload):
 @app.route("/")
 def home():
     """Home"""
-    html = "<h3>Sklearn Prediction Home 🏠</h3>"
+    html = "<h3>Sklearn Prediction Home Page 🏠</h3>"
     return html.format(format)
 
 
